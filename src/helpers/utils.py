4605559dd4c472edb2810e8ff7847ce75bfdba68
schmutov <chmutovsergey@gmail.com>
2023-11-06 11:51:34 +0300

    Extends a methods in the project

---
+++ src/helpers/utils.py
@@ -0,0 +1,11 @@
+import os
+
+from src.models import FileModel
+
+
+def calculate_file_size(files: list[FileModel]) -> int:
+    sum = 0
+    for file in files:
+        sum += os.path.getsize(file.path)
+
+    return sum
--- src/main.py
+++ src/main.py
@@ -1,22 +1,18 @@
+from api.v1 import base, auth
+from config.config import settings
+from config.logger import logger
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
-
-from api.v1 import base
-from config.config import settings
-from config.logger import logger
-# from middleware.blocked_host import BlockedHostMiddleware
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     default_response_class=ORJSONResponse,
 )
 
+app.include_router(auth.api_auth_router)
 app.include_router(base.api_router, prefix='/api/v1')
-
-# app.add_middleware(
-#     BlockedHostMiddleware, blocked_hosts=settings.blocked_hosts
-# )
 
 if __name__ == '__main__':
     logger.info('Server started.')
